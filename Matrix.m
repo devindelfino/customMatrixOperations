1d886f6c25dd77a0c232b41ece3ab872e45ef3ac
Devin Delfino <delfino_devin@wheatoncollege.edu>
2014-11-28 14:05:27 -0500

    finished lu factor method and finding determinants

--- Matrix.m
+++ Matrix.m
@@ -6,7 +6,12 @@ classdef Matrix
 		r = 2;
 		c = 2;
 		data = zeros(2);
-	end %end properties
+	end %end properties (private)
+
+	properties
+		determinant = NaN;
+		invertible = 0;
+	end
 
 	methods
 		% CONSTRUCTOR ------------------------------------------------------------------------------------------
@@ -17,12 +22,21 @@ classdef Matrix
 			if(nargin == 0)
 				M.r = 2;
 				M.c = 2;
-				data = zeros(2);
+				M.data = zeros(2);
+				M.determinant = NaN;
+				M.invertible = 0;
 			end
 			temp_size = size(mat);
 			M.r = temp_size(1);
 			M.c = temp_size(2);
 			M.data = mat;
+			if(det(mat) == 0)
+				M.invertible = 0;
+				M.determinant = 0;
+			else
+				M.invertible = 1;
+			end
+
 		end
 
 		% DISPLAY ------------------------------------------------------------------------------------------
@@ -42,7 +56,12 @@ classdef Matrix
 			dim = [A.r A.c];
 		end
 
+		% GET ELEMENTS ------------------------------------------------------------------------------------------
 		function elmt = get_elements(A, i, j)
+			% Description: Gets an element(s) of a Matrix
+			% Parameters: i - integer or array of integers indicating the rows (: for all)
+			%             j - integer or array of integers indicating the columns (: for all)
+			% Returns: An element or list of elements from the Matrix A 
 			elmt = A.data(i, j);
 		end
 
@@ -126,8 +145,8 @@ classdef Matrix
 				prod_mat = zeros(A_size(1), B_size(2));
 				for(itR = 1:A_size(1))
 					for(itC = 1:B_size(2))
-						size(A.get_elements(itR,:))
-						size(B.get_elements(:,itC))
+						% size(A.get_elements(itR,:))
+						% size(B.get_elements(:,itC))
 						prod_mat(itR, itC) = sum(A.get_elements(itR,:) .* transpose(B.get_elements(:,itC)));
 					end
 				end
@@ -140,26 +159,56 @@ classdef Matrix
 			end
 		end
 
-		% LU FACTORIZATION *WITHOUt PARTIAL PIVOTING ------------------------------------------------------------------------------------------
+		% LU FACTORIZATION WITH PARTIAL PIVOTING ------------------------------------------------------------------------------------------
 		% Used Algorithm from http://www.math.iit.edu/~fass/477577_Chapter_7.pdf, page 66
-		function [U L P]= LU_factor(A) 
-			% Description: Calculates the Upper and Lower triangular matrices of Matrix A along with the permutation Matrix P for stability
+		function [L U P]= LU_factor(A) 
+			% Description: Calculates the Upper and Lower triangular matrices of square Matrix A along with the permutation Matrix P for stability
 			% Parameters: A - a custom Matrix object
 			% Returns: The Upper triangular Matrix (U), the Lower triangular Matrix (L), and the Permutation Matrix (P)
 			dimensions = A.dims();
 			m = dimensions(1); n = dimensions(2);
 
 			if(m == n) % check for a square matrix
-
+				perm_parity = 0;
 				Upper = A.get_elements(:,:); Lower = eye(m); Perm = eye(m);
 				for(k = 1:(m-1))
+
+					for(i = k:m)
+						swap = Upper(k, k:m);
+						Upper(k, k:m) = Upper(i, k:m);
+						Upper(i, k:m) = swap;
+
+						swap = Lower(k, 1:k-1);
+						Lower(k, 1:k-1) = Lower(i, 1:k-1);
+						Lower(i, 1:k-1) = swap;
+
+						swap = Perm(k, :);
+						Perm(k, :) = Perm(i, :);
+						Perm(i, :) = swap;
+						if(i ~= k)
+							perm_parity = perm_parity + 1;
+						end
+					end
+
 					for(j = (k+1):m)
-						Lower(j, k) = Upper(j, k) / Upper(k, k);
+						if(Upper(k, k) == 0)
+							Lower(j, k) = 0;
+						else
+							Lower(j, k) = Upper(j, k) / Upper(k, k);
+						end
 						Upper(j, k:m) = Upper(j, k:m) - Lower(j, k) * Upper(k, k:m);
 						end
 				end
 
+
 				U = Matrix(Upper); L = Matrix(Lower); P = Matrix(Perm);
+
+				if(mod(perm_parity, 2) == 0)
+					P.determinant = 1;
+				else
+					P.determinant = -1;
+				end
+
 			else
 				error('Starting Matrix must be square.')
 			end
@@ -169,18 +218,54 @@ classdef Matrix
 		function M = invert(A) 
 			% Description: Calculates the inverse matrix of Matrix A
 			% Parameters: A - a custom Matrix object
-			% Returns: The inverse matrix or [0] if not invertible 
+			% Returns: The inverse matrix or a matrix of inf values if not-invertible
+			dimensions = A.dims();
+			m = dimensions(1); n = dimensions(2);
 
-			
+			if(m == n) % check for square matrix
+				if(A.invertible)
+					% calculate inverse
+				else
+					display('The matrix is singular because it has a determinant of 0 (not invertible).')
+					M = zeros(m) + inf;
+				end
+			else
+				error('Starting Matrix must be square.')
+			end
 		end
 
 		% DETERMINANT ------------------------------------------------------------------------------------------
 		function determinant = det(A) 
-			% Description: Calculates the determinant of matrix A
+			% Description: Calculates the determinant of matrix A (if already calculated, returns determinant)
 			% Parameters: A - a custom Matrix object
 			% Returns: The determinant of Matrix A
+			dimensions = A.dims();
+			m = dimensions(1); n = dimensions(2);
 
-			
+			if(m == n) % check for square matrix
+				
+				if(isnan(A.determinant))
+					[L U P] = A.LU_factor();
+
+					detP = P.determinant;
+					detL = 1;
+					detU = 1;
+
+					for(i = 1:m) % calculating the product of the diagonal entries of Lower and Upper triangular matrices of A
+						detL = detL * L.get_elements(i,i);
+						detU = detU * U.get_elements(i,i);
+					end
+
+					determinant = detP * detL * detU; % A = P * L * U => det(A) = det(P) * det(L) * det(U)
+					A.determinant = determinant;
+				else
+					determinant = A.determinant;
+				end
+
+			else
+				error('Starting Matrix must be square.')
+			end
 		end
 	end % end methods
+
 end % end classdef
--- test.m
+++ test.m
@@ -1,3 +1,12 @@
 % Filename: test.m
 % Description: Testing environment to validate custom operation implementations against the corresponding built-in MATLAB functions.
 
+
+Am = [2 2 2; 3 3 3; 4 4 4];
+Ac = Matrix(Am);
+
+[Lm Um Pm] = lu(Am);
+[Lc Uc Pc] = Ac.LU_factor();
+
+Bm = magic(5);
+Bc = Matrix(Bm);
